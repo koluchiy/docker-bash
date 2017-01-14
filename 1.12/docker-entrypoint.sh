4a1b194b7a891c3eddad07f56796ff54d08ac1ea
nrybin <n.rybin@carprice.ru>
2017-01-15 00:29:30 +0300

    dind

--- 1.12/docker-entrypoint.sh
+++ 1.12/docker-entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- docker "$@"
+# no arguments passed
+# or first arg is `-f` or `--some-option`
+if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
+	# add our default arguments
+	set -- dockerd \
+		--host=unix:///var/run/docker.sock \
+		--host=tcp://0.0.0.0:2375 \
+		--storage-driver=vfs \
+		"$@"
 fi
 
-# if our command is a valid Docker subcommand, let's invoke it through Docker instead
-# (this allows for "docker run docker ps", etc)
-if docker help "$1" > /dev/null 2>&1; then
-	set -- docker "$@"
-fi
-
-# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
-if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST='tcp://docker:2375'
+if [ "$1" = 'dockerd' ]; then
+	# if we're running Docker, let's pipe through dind
+	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
+	set -- sh "$(which dind)" "$@"
 fi
 
 exec "$@"
